032acc53d2f5a6233e1241305205308114436c1e
jayant <jayantkumar245@gmail.com>
2022-03-03 21:59:19 +0530

    updated file

---
+++ function.js
@@ -0,0 +1,126 @@
+// easy
+      // 1. Given a and b, your function should return the value of ab
+      function power(a, b) {
+        return a ** b;
+      }
+      let result = power(2, 3);
+      console.log(result);
+
+      // 2. Given length of a regular hexagon, your function should return area of the hexagon.
+      function areaofHexagon(a) {
+        return ((3 * 1.732) / 2) * a ** 2;
+      }
+      let result1 = areaofHexagon(10);
+      console.log(result1);
+
+      // 3. Given a sentence, your functions should return the number of words in the sentence.
+      function noOfWords(str) {
+        return str.split(" ").length;
+      }
+      let result3 = noOfWords("i am good person");
+      console.log(result3);
+
+      //  4.  Given n numbers, your function should return the minimum of them all. The number of parameters won't be accepted from user.
+
+      //   5.Given n numbers, your function should return the maximum of them all. The number of parameters won't be accepted from user.
+
+      //   6. Given three angles of a triange, your function should return if it is a scalen, isosceles, equilateral triangle or not a triangle at all.
+      function typeOfTriangle(a, b, c) {
+        if (a == b && b == c) {
+          console.log(`Equilateral Triangle`);
+        } else if (a == b || b == c || c == a) {
+          console.log(`Isosceles Triangle`);
+        } else if ((a != b) != c) {
+          console.log(`Scalene Triangle`);
+        }
+      }
+      let result4 = typeOfTriangle(30, 60, 90);
+
+      // Medium
+      // 1. Given an array, your function should return the length of the array.
+      function arrayLength(arr) {
+        return arr.length;
+      }
+      let result5 = arrayLength([1, 5, 3, 7, 8, 9]);
+      console.log(result5);
+
+      //  2.Given an array and an item, your function should return the index at which the item is present.
+      function findIndex(Array, item) {
+        return Array.indexOf(item);
+      }
+      let res = findIndex([1, 6, 3, 5, 8, 9], 3);
+      console.log(res);
+
+      // function replaceArray(Array, item1, item2) {
+      //   return Array.replace(item1, item2);
+      // }
+      // let res1 = replaceArray([1, 5, 3, 5, 6, 8], 5, 10);
+      // console.log(res1);
+      // function replace(myArray, targetItem, replaceItem) {
+      //   for (let i = 0; i < myArray.length; i++) {
+      //     if (myArray[i] === targetItem) {
+      //       myArray[i] = replaceItem;
+      //       break;
+      //     }
+      //   }
+      // }
+
+      // 4.Given two arrays, your function should return single merged array.
+      function mergearray(arr1, arr2) {
+        return arr1.concat(arr2);
+      }
+      let arr3 = mergearray([1, 3, 5], [2, 4, 6]);
+      console.log(arr3);
+
+      // 5.Given a string and an index, your function should return the character present at that index in the string.
+      function findCharAt(str, index) {
+        return str.charAt(index);
+      }
+      let res2 = findCharAt("skillsafari", 5);
+      console.log(res2);
+
+      //   advanced
+      // 1.Write a function which generates a secret code from a given string, by shifting characters of alphabet by N places.
+
+      // 2.Given a sentence, return a sentence with first letter of all words as capital.
+      function toSentenceCase(str) {
+        const words = str.split(" ");
+        for (let i = 0; i < words.length; i++) {
+          words[i] = words[i][0].toUpperCase() + words[i].substr(1);
+        }
+        return words.join(" ");
+      }
+      let res3 = toSentenceCase("we are alchemyst");
+      console.log(res3);
+
+      // const toTitleCase = (phrase) => {
+      //   return phrase
+      //     .toLowerCase()
+      //     .split(" ")
+      //     .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+      //     .join(" ");
+      // };
+      // let result = toTitleCase("maRy hAd a lIttLe LaMb");
+      // console.log(result);
+
+      // 3.Given an array of numbers, your function should return an array in the ascending order.
+
+      function sortArray(array) {
+        return array.sort(function (a, b) {
+          return a - b;
+        });
+      }
+      let res4 = sortArray([100, 83, 32, 9, 45, 61]);
+      console.log(res4);
+      // 4.Given a sentence, your function should reverse the order of characters in each word, keeping same sequence of words.
+      function reverseCharactersOfWord(string) {
+        return string
+          .split(" ")
+          .map(function (word) {
+            return word.split("").reverse().join("");
+          })
+          .join(" ");
+      }
+      let res5 = reverseCharactersOfWord("we are alchemyst");
+      console.log(res5);
+      // weN ,gnirts emas .stluser
---
+++ number.js
@@ -0,0 +1,78 @@
+// 1.Write a program to input 2 numbers and display the sum of the numbers to the console.
+// let number1 = 20,
+//   number2 = 40;
+// let sum = number1 + number2;
+// console.log(`sum: ${sum}`);
+
+// 2.Write a JavaScript program to calculate the simple interest given P,R,T with the given formula. Formula: SI = (P * T * R) / 100 Where: P = principal amount T = time R = rate SI = simple interest
+let p = 100,
+  t = 2,
+  r = 6,
+  SI = (p * r * t) / 100;
+console.log(SI);
+
+// 3.Write a program to calculate the kinetic energy of a body with mass 'm' and volume 'v'.
+let m = 12,
+  v = 2,
+  Formula = 0.5 * m * v * v;
+console.log(Formula);
+
+// 4.Write a program to convert Fahrenheit to Celsius. For Fahrenheit to Celsius conversion use: T = (T - 32) 9/5 'T' is the temperature on the Fahrenheit scale.
+let temp = 56;
+let result = (temp - 32) * (5 / 9);
+console.log(result);
+
+// 5.Calculate the area, perimeter of a s1.re of side 'a'. Also, calculate the surface area and the volume of a cube of side 'a'.
+let a = 4,
+  Area = a * a,
+  Perimeter = 4 * a,
+  Surfacearea = 6 * a * a,
+  Volume = a * a * a;
+console.log(`${Area},${Perimeter},${Surfacearea},${Volume}`);
+
+// 6.Given the Cost Price(CP) and Selling Price(SP) of a product. Write a Program to Calculate the Profit or Loss.
+let cp = 1500,
+  sp = 2000;
+Profit = sp - cp;
+console.log(`${Profit} Profit`);
+
+let CP = 3125,
+  SP = 1125;
+loss = CP - SP;
+console.log(`${loss} Loss`);
+
+// 7.Write a program to calculate sum of N natural digits, as input by the users?
+// let naturalNum = prompt(`Enter a positive Integer: `);
+// let sum = 0;
+// for (let i = 1; i <= naturalNum; i++) {
+//   sum += i;
+// }
+// console.log(`Sum: ${sum}`);
+
+// 8.Write a Program to Print N Odd Number in Descending Order.
+// let n = parseInt(prompt("Enter a number:"));
+// for (i = n - 1; i >= 0; i--) {
+//   a = 1 + i * 2;
+//   console.log(a);
+// }
+
+// 9.Write a JavaScript program to compute the sum of all digits that occur in a given string.
+// let value = 2568,
+//   sum = 0;
+// while (value) {
+//   sum += value % 10;
+//   value = Math.floor(value / 10);
+// }
+// console.log(sum);
+
+// 10.Write a JavaScript program that reverses a number.
+// let num=456789;
+// reverse=num.toString().split("").reverse().join("")
+// console.log(reverse)
+
+// program to convert decimal to binary
+
+
+const number = parseInt(prompt("Enter a decimal number: "));
+const result1 = number.toString(2);
+console.log("Binary:" + " " + result1);
